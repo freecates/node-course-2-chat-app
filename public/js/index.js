56e3ea2aa6e3c5374a08388d4eaa5f9b14d0417f
freecates <ramongil@ramongil.cat>
2017-02-20 11:19:58 +0100

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -3,10 +3,6 @@ var socket = io();
 socket.on('connect', function () {
     console.log('Connected to server');
 
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Hey. This is Ramon'
-    });
 });
 
 socket.on('disconnect', function () {

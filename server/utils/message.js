825393fafeab5ee85607a5190271a6aeb2d44c62
freecates <ramongil@ramongil.cat>
2017-02-21 17:12:58 +0100

    Add mustachejs for message templates

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,12 +1,10 @@
 var moment = require('moment');
 
-var createdAt = moment().valueOf();
-
 var generateMessage = (from, text) => {
     return {
         from,
         text,
-        createdAt
+        createdAt: moment().valueOf()
     };
 };
 
@@ -14,7 +12,7 @@ var generateLocationMessage = (from, latitude, longitude) => {
     return {
         from,
         url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-        createdAt
+        createdAt: moment().valueOf()
     }
 };
 
